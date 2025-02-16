29069a984e2e4f9bac4d8268455a6ffc3eed161a
Milan-Gajic2001 <156429209+Milan-Gajic2001@users.noreply.github.com>
2025-02-16 12:59:58 +0100

    add getSuppliers to ProductController

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     //
+    public function getSuppliers()
+    {
+        $supplierNames = Product::distinct()->pluck('supplier_name');
+
+        return response()->json($supplierNames);
+    }
 }
