27925afb3efd77147ae9773b6c241f32825d3975
Milan-Gajic2001 <156429209+Milan-Gajic2001@users.noreply.github.com>
2025-02-16 13:47:47 +0100

    add button for deleting product

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,6 +4,12 @@
         <h4>Part number: {{ $p->part_number }}</h4>
         <h4>Part description: {{ $p->part_desc }}</h4>
         <br>
+        <form action="/api/products/{{ $p->id }}" method="POST">
+            @method('DELETE')
+            @csrf
+            <button type="submit">Delete product</button>
+        </form>
+        <br>
         <a href="/products/{{ $p->id }}"><button>Change product data</button></a>
         <hr>
     @endforeach
