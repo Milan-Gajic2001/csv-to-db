8c31c3eb3774afe609468455f99afb7e04863529
Milan-Gajic2001 <156429209+Milan-Gajic2001@users.noreply.github.com>
2025-02-16 13:02:12 +0100

    add route for showing all suppliers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('api')->group(function () {
+    // RADI
+    Route::get('/suppliers', [ProductController::class, 'getSuppliers']);
 });
