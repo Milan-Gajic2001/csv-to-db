c4bcb61c5e85a2681c8e082c7e4fd23f511db477
Milan-Gajic2001 <156429209+Milan-Gajic2001@users.noreply.github.com>
2025-02-16 12:42:24 +0100

    add import handler and fix model table

--- app/Console/Commands/ImportProducts.php
+++ app/Console/Commands/ImportProducts.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Product;
 use Illuminate\Console\Command;
 
 class ImportProducts extends Command
@@ -11,20 +12,54 @@ class ImportProducts extends Command
      *
      * @var string
      */
-    protected $signature = 'import-products {file}';
+    protected $signature = 'import:products {file}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'This commands insert data into products table from csv file';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $filePath = $this->argument("file");
+
+        //Checking if file exists, if not return error with message
+        if (!file_exists($filePath) || !is_readable($filePath)) {
+            $this->error('File does not exist or is not readable.');
+            return;
+        }
+        // file method makes array of csv file 
+        // collect method makes collection of array
+        $data = collect(file($filePath))
+            ->skip(1)
+            // each data in one line separated by coma is made into array
+            ->map(fn($line) => str_getcsv($line))
+            ->map(fn($productData) => [
+                'supplier_name' => $productData[0],
+                'days_valid' => intval($productData[1]),
+                'priority' => intval($productData[2]),
+                'part_number' => $productData[3],
+                'part_desc' => $productData[4],
+                'quantity' => intval($productData[5]),
+                'price' => intval($productData[6]),
+                'condition' => $productData[7],
+                'category' => $productData[8],
+            ])
+            // Checking if row has all needed data
+            ->filter(function ($productData) {
+                foreach ($productData as $value) {
+                    if (!$value) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+
+        Product::insert($data->all());
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
     //
     use HasFactory;
 
-    protected $table = 'suppliers';
+    protected $table = 'products';
 
     protected $fillable = [
         'supplier_name',
